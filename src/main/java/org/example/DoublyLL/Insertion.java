bae0da5ed2ea6eb139504c5c5a70e4ce013a7cb4
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-15 20:27:51 +0500

    Count_No_Node

--- src/main/java/org/example/DoublyLL/Insertion.java
+++ src/main/java/org/example/DoublyLL/Insertion.java
@@ -1,6 +1,20 @@
 package org.example.DoublyLL;
 
 public class Insertion {
+    Node head;
+    Node tail;
+    class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+
+    }
 
     public static void main(String[] args) {
 

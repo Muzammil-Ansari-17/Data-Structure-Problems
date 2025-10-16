c2d8a6580d3b2d07ba5a40867bf9e077e3dcf9e8
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-16 20:16:12 +0500

    HospitalManagement

--- src/main/java/org/example/DoublyLL/HospitalManagement.java
+++ src/main/java/org/example/DoublyLL/HospitalManagement.java
@@ -1,9 +1,7 @@
 package org.example.DoublyLL;
 
 
-
-public class HospitalManagement {
-    class patient{
+    class patient {
         int p_id;
         String p_name;
         String adm_date;
@@ -11,7 +9,7 @@ public class HospitalManagement {
         int severity;
         patient next;
 
-        public patient(int p_id,String p_name , String adm_date,String ty_illness,int severity){
+        public patient(int p_id, String p_name, String adm_date, String ty_illness, int severity) {
             this.p_id = p_id;
             this.p_name = p_name;
             this.adm_date = adm_date;
@@ -19,8 +17,10 @@ public class HospitalManagement {
             this.severity = severity;
             this.next = null;
         }
-        class patientlist{
+    }
+        class patientlist {
             patient head ;
+
 
             public void addsevernity(patient newpatient){
                 if(head == null || newpatient.severity > head.severity){
@@ -36,6 +36,33 @@ public class HospitalManagement {
                 }
             }
 
-        }
+            public void display(){
+                patient current = head;
+                while(current != null){
+                    System.out.println("-------------------------");
+                    System.out.println("_____Patient Detail______");
+                    System.out.println("-------------------------");
+                    System.out.println("Id : " +current.p_id);
+                    System.out.println("Patient name : "+current.p_name);
+                    System.out.println("Admission data : "+current.adm_date);
+                    System.out.println("Illness : "+current.ty_illness);
+                    System.out.println("Severity level : "+current.severity);
+                    current = current.next;
+                }
+
+            }
+
+         }
+public class HospitalManagement {
+
+    public static void main(String[] args) {
+        patientlist patient = new patientlist();
+        patient.addsevernity(new patient(1,"Muzammil","17-11-2024","fever",1));
+        patient.addsevernity(new patient(2,"ali","18-10-2024","brain tumor",4));
+        patient.addsevernity(new patient(3,"john","18-09-2024","cancer",5));
+        patient.addsevernity(new patient(4,"ahmed","14-08-2024","dengue",4));
+        patient.addsevernity(new patient(5,"jack","12-07-2024","flu",2));
+        patient.display();
+
     }
 }

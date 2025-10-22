0a888e5d606a6e4dc690e65f8a224ec05dbab125
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-22 23:30:40 +0500

    feat update

--- src/main/java/org/example/Array/Input_2d_Array.java
+++ src/main/java/org/example/Array/Input_2d_Array.java
@@ -1,4 +1,10 @@
 package org.example.Array;
 
+import java.util.Scanner;
+
 public class Input_2d_Array {
+    public static void main(String[] args) {
+        int arr[][] = new int[5][5];
+        Scanner sc = new Scanner(System.in);
+    }
 }

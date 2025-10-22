af202b088a3ffd6f5ce88a91a17cd51b8c3b45df
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-22 23:42:26 +0500

    feat update

---
+++ src/main/java/org/example/Array/Avg_array.java
@@ -0,0 +1,12 @@
+package org.example.Array;
+
+import java.util.Scanner;
+
+public class Avg_array {
+
+    public static void main(String[] args) {
+        int arr[][] = new int[5][5];
+
+    }
+
+}
--- src/main/java/org/example/Array/Input_2d_Array.java
+++ src/main/java/org/example/Array/Input_2d_Array.java
@@ -6,5 +6,13 @@ public class Input_2d_Array {
     public static void main(String[] args) {
         int arr[][] = new int[5][5];
         Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+                System.out.print("Enter element "+i+" "+j+" :");
+                    arr[i][j] = sc.nextInt();
+
+            }
+
+        }
     }
 }

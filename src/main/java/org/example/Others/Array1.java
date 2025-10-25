ea0a0fbeacaf2b603129b7cfd02a47f797bbb0e5
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-25 19:10:04 +0500

    feat update

--- src/main/java/org/example/Others/Array1.java
+++ src/main/java/org/example/Others/Array1.java
@@ -6,15 +6,33 @@ public class Array1 {
 
     public static void main(String[] args) {
 
-        double[] temperature = new double[7];
+        double[] temp = new double[7];
         Scanner sc = new  Scanner(System.in);
-        String[] days = {"Monday, Tuesday, Wednesday, Thursday , Friday, Saturday,Sunday"};
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday" , "Friday", "Saturday", "Sunday"};
 
 
         for (int i = 0; i < days.length; i++) {
-            System.out.println(days[i] + " ");
-            temperature[i] = sc.nextDouble();
+            System.out.print(days[i] + " ");
+            temp[i] = sc.nextDouble();
         }
+
+        double maxtemp = temp[0];
+        double mintemp = temp[0];
+        int maxdayindex = 0;
+        int mindayindex = 0;
+
+        for (int i = 0; i < temp.length; i++) {
+            if(temp[i] > maxtemp){
+                maxtemp = temp[i];
+                maxdayindex = i;
+            }
+            if(temp[i] < mintemp){
+                mintemp = temp[i];
+                mindayindex = i;
+            }
+        }
+        System.out.println("Highest temp was on "+days[maxdayindex]+ " :"+maxtemp);
+        System.out.println("Minnimum temp was on "+days[mindayindex]+ " :"+mintemp);
 
     }
 }

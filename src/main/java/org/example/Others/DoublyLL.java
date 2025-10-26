5e47566a5351c7c826e08a341ca369b65f29bfe1
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-10-26 17:33:54 +0500

    feat update

---
+++ src/main/java/org/example/Others/DoublyLL.java
@@ -0,0 +1,95 @@
+package org.example.Others;
+
+public class DoublyLL {
+        Node head;
+        Node tail;
+       public class Node{
+                int data;
+                Node next;
+                Node prev;
+
+            Node(int data){
+                this.data = data;
+                this.next = null;
+                this.prev = null;
+            }
+        }
+
+        public void addfirst(int data){
+           Node newnode = new Node(data);
+           if(head == null ){
+               head = newnode;
+               tail = newnode;
+           }else{
+               newnode.next  =  head;
+               head.prev = newnode;
+               head = newnode;
+           }
+        }
+
+        public void addlast(int data){
+           Node newnode = new Node(data);
+           if(head == null){
+               head = newnode;
+               tail = newnode;
+           }else{
+                tail.next = newnode;
+                newnode.prev = tail;
+                tail = newnode;
+              }
+       }
+
+       public void removefirst(){
+           if(head == null){
+               System.out.println("list does not exist");
+               return;
+           }else{
+               head= head.next;
+           }
+       }
+
+       public void removelast(){
+           if(head == null){
+               System.out.println("list does not exist");
+               return;
+           }else{
+               Node current = head;
+               while(current.next.next != null){
+                   current = current.next;
+               }
+               current.next = null;
+               tail = current;
+           }
+
+       }
+
+        public void display(){
+           if(head == null){
+               System.out.println("list does not exist");
+               return;
+           }else{
+               Node current =head;
+               while(current != null){
+                   System.out.print(" <-> "+current.data);
+                   current = current.next;
+               }
+           }
+        }
+
+    public static void main(String[] args) {
+        DoublyLL obj = new DoublyLL();
+        obj.addfirst(56);
+        obj.addfirst(57);
+        obj.display();
+        System.out.println();
+        obj.addlast(90);
+        obj.display();
+        System.out.println();
+        obj.removefirst();
+        obj.display();
+        System.out.println();
+        obj.removelast();
+        obj.display();
+
+    }
+}

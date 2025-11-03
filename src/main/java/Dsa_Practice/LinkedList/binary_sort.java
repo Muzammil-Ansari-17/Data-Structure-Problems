7bfb6e0d466383d84c43233448281ee7fd130592
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-11-04 01:57:34 +0500

    Remove Duplicate in SLL

---
+++ src/main/java/Dsa_Practice/LinkedList/binary_sort.java
@@ -0,0 +1,66 @@
+package Dsa_Practice.LinkedList;
+
+public class binary_sort {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head;
+    public void addfirst(int data){
+        Node newnode = new Node(data);
+        if(head == null){
+            head = newnode;
+        }else {
+            newnode.next = head;
+            head = newnode;
+        }
+    }
+
+    public void bubblesort(){
+        Node current = head;
+        boolean swapped;
+        do{
+            swapped = false;
+            while(current.next != null){
+                if(current.data > current.next.data){
+                    int temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+        }
+        while (swapped);
+    }
+
+    public  void display(){
+        if(head == null){
+            System.out.println("list doesnot exist.");
+            return;
+        }else{
+            Node current = head;
+            while(current != null){
+                System.out.print(current.data+" -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+    }
+    public static void main(String[] args) {
+            binary_sort obj = new binary_sort();
+            obj.addfirst(22);
+        obj.addfirst(76);
+        obj.addfirst(91);
+        obj.addfirst(81);
+        obj.addfirst(-1);
+        obj.display();
+        obj.bubblesort();
+        obj.display();
+    }
+}

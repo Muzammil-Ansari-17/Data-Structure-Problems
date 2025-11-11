6423dad939926b402aa46c59dab4e2092a06d7ba
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-11-11 11:38:47 +0500

    Missing_Pop

---
+++ src/main/java/Dsa_Practice/Stack/Missing_Pop.java
@@ -0,0 +1,24 @@
+package Dsa_Practice.Stack;
+
+public class Missing_Pop {
+
+//    public static class Stack(){
+//        int top = -1;
+//        int arr[] = new int[5];
+//
+//        void push(int data){
+//            if(top == arr.length){
+//                System.out.println("Stack overflow");
+//            }else{
+//                arr[top++] = data;
+//             }
+//            int pop()
+//        }
+//    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}

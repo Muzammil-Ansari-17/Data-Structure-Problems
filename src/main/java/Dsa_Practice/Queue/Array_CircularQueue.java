41475f11bc97a02225efd63e6095e835bd4f51f6
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-11-20 20:31:03 +0500

    Circular Queue

---
+++ src/main/java/Dsa_Practice/Queue/Array_CircularQueue.java
@@ -0,0 +1,20 @@
+package Dsa_Practice.Queue;
+
+public class Array_CircularQueue {
+    public class Queue{
+        int[] arr;
+        int rear;
+        int front;
+        int max;
+
+        Queue(int sizeofQ){
+            this.arr = new int[sizeofQ];
+            this.front = rear-1;
+            this.max = sizeofQ;
+
+        }
+
+        
+
+    }
+}

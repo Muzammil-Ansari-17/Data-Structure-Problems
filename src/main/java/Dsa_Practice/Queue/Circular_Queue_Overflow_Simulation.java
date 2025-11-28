5c2fae2d6a1d50b3a4dcf4fa15d503984df798ca
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-11-28 20:13:26 +0500

    Circular_Queue_Overflow_Simulation

--- src/main/java/Dsa_Practice/Queue/Circular_Queue_Overflow_Simulation.java
+++ src/main/java/Dsa_Practice/Queue/Circular_Queue_Overflow_Simulation.java
@@ -5,31 +5,85 @@
 package Dsa_Practice.Queue;
 
 public class Circular_Queue_Overflow_Simulation {
-    public static class C_Queue{
+    public static class C_Queue {
+
         String[] arr;
         int front;
         int rear;
         int max = 5;
 
-        C_Queue(){
+        C_Queue() {
             this.arr = new String[max];
             this.front = -1;
             this.rear = -1;
         }
 
-        public boolean isEmpty(){
-            return front= -1;
+        public boolean isEmpty() {
+            return front == -1;
         }
 
-        public boolean isFull(){
-            return rear = (rear + 1) % max = front;
+        public boolean isFull() {
+            return (rear + 1) % max == front;
         }
-        public void enque(String Patient_Name){
 
+        public void enqueue(String patient_Name) {
+            if (isFull()) {
+                System.out.println("Mr/Ms "+patient_Name+" waiting area is Full.");
+                return;
+            } else {
+                if (isEmpty()) {
+                    front = 0;
+                }
+                rear = (rear + 1) % max;
+                arr[rear] = patient_Name;
+            }
+        }
+
+        public void dequeue(){
+            if(isEmpty()){
+                System.out.println("Waiting area is Empty.");
+                return;
+            }
+            String removed = arr[front];
+
+            if(front == rear){
+                front = rear = -1;
+            }else{
+                front = (front + 1 ) % max;
+            }
+            System.out.println("\nPatient "+removed+ " left the waiting area.");
+        }
+
+        public void display(){
+            if(isEmpty()){
+                System.out.println("Waiting area is Empty.");
+                return;
+            }else{
+                System.out.println("\nPatient in waiting area are...");
+
+                int i = front;
+                while(true){
+                    System.out.println(arr[i]+" ");
+                    if(i == rear){
+                        break;
+                    }
+                    i = (i + 1) % max;
+                }
+                System.out.println();
+            }
         }
     }
 
     public static void main(String[] args) {
-
+            C_Queue queue = new C_Queue();
+            queue.enqueue("Muzammil");
+            queue.enqueue("Ahmed");
+            queue.enqueue("Ali");
+            queue.enqueue("Ibrahim");
+            queue.enqueue("Ahmed");
+            queue.display();
+            queue.enqueue("Sara");
+            queue.dequeue();
+            queue.display();
     }
 }

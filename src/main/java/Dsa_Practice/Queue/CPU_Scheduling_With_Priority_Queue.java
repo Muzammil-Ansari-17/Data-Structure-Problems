b723046c45ae0adbf253381d7f5be9334e02c81a
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-11-29 20:25:53 +0500

    CPU_Scheduling_With_Priority_Queue

---
+++ src/main/java/Dsa_Practice/Queue/CPU_Scheduling_With_Priority_Queue.java
@@ -0,0 +1,35 @@
+package Dsa_Practice.Queue;
+import java.util.PriorityQueue;
+
+public class CPU_Scheduling_With_Priority_Queue {
+
+        public static class CPU_P{
+            String name;
+            int priority;
+
+            CPU_P(String name, int priority){
+                this.name = name;
+                this.priority = priority;
+            }
+
+            public String toSring() {
+                return name;
+            }
+        }
+
+    public static void main(String[] args) {
+            PriorityQueue<CPU_P> pqueue = new PriorityQueue<>((a,b) -> a.priority - b.priority);
+
+            pqueue.offer(new CPU_P("P2",1));
+            pqueue.offer(new CPU_P("P1",3));
+            pqueue.offer(new CPU_P("P3",2));
+
+            PriorityQueue<CPU_P> Display = new PriorityQueue<>(pqueue);
+
+            while(!Display.isEmpty()){
+                System.out.print(Display.poll().toSring());
+                System.out.print(" -> ");
+            }
+
+    }
+}

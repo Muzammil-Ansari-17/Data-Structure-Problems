8645af2df6b7d42d97f91255ade9b33395e656da
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-11-30 19:17:50 +0500

    Task Manager Using Priority Queue

--- src/main/java/Dsa_Practice/Queue/Task_Manager_Using_Priority_Queue.java
+++ src/main/java/Dsa_Practice/Queue/Task_Manager_Using_Priority_Queue.java
@@ -1,15 +1,44 @@
 package Dsa_Practice.Queue;
 
+import java.util.PriorityQueue;
+
 public class Task_Manager_Using_Priority_Queue {
-    class TaskManager{
-        String[] Task;
+
+    public static class TaskManager{
+        String Task;
         int priority;
 
-        TaskManager(){
+        TaskManager(String Task , int priority){
+                this.Task = Task;
+                this.priority = priority;
+        }
 
+        @Override
+        public String toString(){
+            return Task+" (Priority: "+priority+")";
         }
     }
     public static void main(String[] args) {
-        Priority_Queue queue = new Priority_Queue((a,b) -> a-b);
+
+        PriorityQueue<TaskManager> task = new PriorityQueue<>((a,b) -> a.priority - b.priority);
+        task.offer(new TaskManager("Finish Dsa Assignment",1));
+        task.offer(new TaskManager("Check Email",5));
+        task.offer(new TaskManager("Make Presentation",2));
+
+        int i = 1;
+        System.out.println("\nTask list");
+        for (TaskManager t: task){
+            System.out.println(i+": "+t);
+            i++;
+        }
+
+        System.out.println("\nCompleted Task : "+task.poll());
+
+        System.out.println("\nRemaining Task");
+        i = 1;
+        for(TaskManager t: task){
+            System.out.println(i+": "+t);
+            i++;
+        }
     }
 }

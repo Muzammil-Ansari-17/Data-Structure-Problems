7bb3deb3f905037cfac0d0a0400b8fbaea85861a
Muzammil-Ansari-17 <muzammilahmed0888@gmail.com>
2025-11-30 18:31:44 +0500

    Priority Queue TaskManager

---
+++ src/main/java/Dsa_Practice/Queue/Task_Manager_Using_Priority_Queue.java
@@ -0,0 +1,15 @@
+package Dsa_Practice.Queue;
+
+public class Task_Manager_Using_Priority_Queue {
+    class TaskManager{
+        String[] Task;
+        int priority;
+
+        TaskManager(){
+
+        }
+    }
+    public static void main(String[] args) {
+        Priority_Queue queue = new Priority_Queue((a,b) -> a-b);
+    }
+}

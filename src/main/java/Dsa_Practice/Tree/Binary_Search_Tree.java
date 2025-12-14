6c7d3b0b7780f831a1ffa675eceaf083f425671f
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-12-15 04:45:43 +0500

    BST

--- src/main/java/Dsa_Practice/Tree/Binary_Search_Tree.java
+++ src/main/java/Dsa_Practice/Tree/Binary_Search_Tree.java
@@ -28,12 +28,62 @@ public class Binary_Search_Tree {
         return root;
     }
 
+    public static boolean search(Node root,int key){
+        if(root == null){
+            return false;
+        }else if(root.data > key){
+            return search(root.left,key);
+        }else if(root.data < key){
+            return search(root.right,key);
+        }else if(root.data == key){
+            return true;
+        }
+        return false;
+    }
+
+    public static Node Delete(Node root,int value){
+        if(root == null){
+            System.out.println("Empty");
+        }
+        else if (root.data > value){
+            root.left = Delete(root.left,value);
+        }
+        else if (root.data < value) {
+            root.right = Delete(root.right,value);
+        }
+        else if(root.data == value){
+//                case 1
+            if(root.left == null && root.right == null ){
+                return null;
+            }
+//            case 2
+            if(root.left == null){
+                return root.right;
+            }
+            else if (root.right == null) {
+                return root.left;
+            }
+//            case 3
+            Node IS = InorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = Delete(root.right,value);
+        }
+        return root;
+    }
+
+    public static Node InorderSuccessor(Node root){
+        while (root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void inorder(Node root){
         if(root == null){
             return;
         }else {
             inorder(root.left);
-            System.out.println(root.data+" ");
+            System.out.print(root.data+" ");
             inorder(root.right);
         }
     }
@@ -47,6 +97,18 @@ public class Binary_Search_Tree {
         }
         inorder(root);
         System.out.println();
+
+
+        if(search(root,1)){
+            System.out.println("Found");
+        }else {
+            System.out.println("Not Found");
+        }
+
+        System.out.println("Delete Node");
+        root = Delete(root,5);
+        inorder(root);
+
     }
 }
 

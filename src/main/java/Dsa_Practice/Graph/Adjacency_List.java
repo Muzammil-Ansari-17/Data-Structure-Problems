f5e2922c42bc907293489f714fd7feae7632ab9c
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2025-12-24 02:02:14 +0500

    Adjacency_List

--- src/main/java/Dsa_Practice/Graph/Adjacency_List.java
+++ src/main/java/Dsa_Practice/Graph/Adjacency_List.java
@@ -1,36 +1,50 @@
-package Dsa_Practice.Graph;
-
 import java.util.ArrayList;
 
 public class Adjacency_List  {
-    public static class edge{
-        int source;
-        int dest;
+        public static class Edge{
+            int source;
+            int destination;
 
-        edge(int s, int d){
-            this.source = s;
-            this.dest = d;
+            Edge(int s, int d){
+               this.source = s;
+               this.destination = d;
+            }
         }
-    }
-    public static void createGraph(ArrayList<edge>Graph[]){
-        for (int i = 0; i < Graph.length; i++) {
-            Graph[i] = new ArrayList<edge>();
+        public static void createGraph(ArrayList<Edge>[] graph){
+            for (int i = 0; i < graph.length; i++) {
+                    graph[i] = new ArrayList<Edge>();
+            }
+
+            graph[0].add(new Edge(0,1));
+            graph[0].add(new Edge(0,2));
+            graph[0].add(new Edge(0,3));
+
+            graph[1].add(new Edge(1,0));
+            graph[1].add(new Edge(1,3));
+
+            graph[2].add(new Edge(2,0));
+            graph[2].add(new Edge(2,3));
+            graph[2].add(new Edge(2,4));
+            graph[2].add(new Edge(2,5));
+
+            graph[3].add(new Edge(3,0));
+            graph[3].add(new Edge(3,1));
+            graph[3].add(new Edge(3,2));
+
+            graph[4].add(new Edge(4,2));
+
+            graph[5].add(new Edge(5,2));
         }
-        Graph[0].add(new edge(0,2));
 
-        Graph[1].add(new edge(1,2));
-        Graph[1].add(new edge(1,3));
 
-        Graph[2].add(new edge(2,0));
-        Graph[2].add(new edge(2,1));
-        Graph[2].add(new edge(2,3));
-
-        Graph[3].add(new edge(3,1));
-        Graph[3].add(new edge(3,2));
-    }
     public static void main(String[] args) {
-            int V = 4;
+        int v = 6;
+        ArrayList<Edge>[] graph =  new ArrayList[v];
+        createGraph(graph);
 
-        ArrayList<edge> Graph[] = new ArrayList<>(V);
+        for (int i = 0; i < graph[2].size(); i++) {
+            Edge e = graph[2].get(i);
+            System.out.println(e.destination+" ");
+        }
     }
 }

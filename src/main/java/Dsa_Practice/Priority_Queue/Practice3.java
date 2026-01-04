bae0af655716c0789a796674e17ccadba300c7d5
Muzammil Ahmed <muzammilahmed0888@gmail.com>
2026-01-04 23:34:18 +0500

    Multiple folder added

--- src/main/java/Dsa_Practice/Priority_Queue/Practice3.java
+++ src/main/java/Dsa_Practice/Priority_Queue/Practice3.java
@@ -11,12 +11,20 @@ public class Practice3 {
             this.name = name;
             this.marks = marks;
         }
+
+        public String toString(){
+            return name+" "+ marks;
+        }
     }
 
     public static void main(String[] args) {
-        PriorityQueue<PQ> pq = new PriorityQueue<>((a,b) -> a.marks - b.marks);
+        PriorityQueue<PQ> pq = new PriorityQueue<>((a,b) -> b.marks - a.marks);
         pq.add(new PQ("Muzammil",98));
-        pq.add(new PQ("Ahmed",98));
-        pq.add(new PQ("Faraz",98));
+        pq.add(new PQ("Ahmed",100));
+        pq.add(new PQ("Faraz",99));
+
+        while (!pq.isEmpty()){
+            System.out.println(pq.poll());
+        }
     }
 }
